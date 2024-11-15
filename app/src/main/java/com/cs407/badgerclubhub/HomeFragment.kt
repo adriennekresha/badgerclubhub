b84fcd390509b2e5b46629b4a650b4f17e2279c0
lturf <lauren.turf@icloud.com>
2024-11-14 20:02:28 -0600

    updated menu and nav graph

--- app/src/main/java/com/cs407/badgerclubhub/HomeFragment.kt
+++ app/src/main/java/com/cs407/badgerclubhub/HomeFragment.kt
@@ -2,14 +2,20 @@ package com.cs407.badgerclubhub
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 
-class HomeFragment : Fragment(){
-    private lateinit var discoverButton:Button
+class HomeFragment : Fragment() {
+    private lateinit var discoverButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,4 +28,25 @@ class HomeFragment : Fragment(){
         }
         return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.bottom_menu, menu);
+    }
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.search -> {
+                findNavController().navigate(R.id.action_home_search_to_search)
+                true
+            }
+
+            R.id.schedule -> {
+                findNavController().navigate(R.id.action_home_schedule_to_schedule)
+                true
+            }
+
+            else -> false
+        }
+    }
+
 }
--- app/src/main/java/com/cs407/badgerclubhub/MainActivity.kt
+++ app/src/main/java/com/cs407/badgerclubhub/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.cs407.badgerclubhub
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,34 +18,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.bottom_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.home -> {
-                // replaceFragment(HomeFragment)
-                true
-            }
-            R.id.search -> {
-                // replaceFragment(SearchFragment)
-                true
-            }
-            R.id.schedule -> {
-                // replaceFragment(ScheduleFragment)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    // https://www.youtube.com/watch?v=jOFLmKMOcK0 (bottom menu that switches fragments)
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = getSupportFragmentManager()
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
-    }
 }
